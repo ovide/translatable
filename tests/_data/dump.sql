9cb9e9adbe46552b6d0560d9eb9d8856e64ad155
Albert Ovide <albert.ovide@igmweb.com>
2014-11-21 20:37:51 +0000

    Add backend tables translation and translation2.translation

--- tests/_data/dump.sql
+++ tests/_data/dump.sql
@@ -1,9 +1,12 @@
+USE translatable;
 DROP TABLE IF EXISTS basic;
 DROP TABLE IF EXISTS table1;
 DROP TABLE IF EXISTS table2;
 DROP TABLE IF EXISTS table3;
+DROP TABLE IF EXISTS translation;
 DROP TABLE IF EXISTS translatable2.table1;
 DROP TABLE IF EXISTS translatable2.table2;
+DROP TABLE IF EXISTS translatable2.translation;
 
 CREATE TABLE basic (
   id INT NOT NULL AUTO_INCREMENT,
@@ -31,3 +34,20 @@ CREATE TABLE translatable2.table2 (
   id INT NOT NULL AUTO_INCREMENT,
   t1_id INT NOT NULL,
   PRIMARY KEY (id));
+
+
+CREATE TABLE translation (
+  `table` VARCHAR(255) NOT NULL,
+  field VARCHAR(255) NOT NULL,
+  row VARCHAR(255) NOT NULL,
+  lang CHAR(2) NOT NULL,
+  text TEXT NOT NULL,
+  PRIMARY KEY (`table` ASC, field ASC, row ASC, lang ASC));
+
+CREATE TABLE translatable2.translation (
+  `table` VARCHAR(255) NOT NULL,
+  field VARCHAR(255) NOT NULL,
+  row VARCHAR(255) NOT NULL,
+  lang CHAR(2) NOT NULL,
+  text TEXT NOT NULL,
+  PRIMARY KEY (`table` ASC, field ASC, row ASC, lang ASC));
