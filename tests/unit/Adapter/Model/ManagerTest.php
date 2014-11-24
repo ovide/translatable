65ac3d6d5c1b423576424084b1568f210ddfb38d
Albert Ovide <albert.ovide@igmweb.com>
2014-11-24 11:25:17 +0000

    Unit test OK

--- tests/unit/Adapter/Model/ManagerTest.php
+++ tests/unit/Adapter/Model/ManagerTest.php
@@ -66,7 +66,7 @@ class ManagerTest extends \Codeception\TestCase\Test
 	{
 		$I = $this->tester;
 
-		$expected = 'hi there';
+		$expected = 'foo bar';
 		$record   = 1;
 		$field    = 'name';
 		$language = 'en';
@@ -100,20 +100,185 @@ class ManagerTest extends \Codeception\TestCase\Test
 				['backendModel' => $backendClassName]
 		);
 		$this->assertEmpty($manager2->get($field, $language));
+
+		$I->dropFromDB('translation');
 	}
 
+	/**
+	 * @depends testGet
+	 */
 	public function testSet()
 	{
+		$I = $this->tester;
 
+		$record   = 1;
+		$field    = 'name';
+		$language = 'en';
+
+		$I->haveInDatabase('basic', ['id' => $record]);
+		$I->haveInDatabase('translation', [
+			'table' => \Mocks\Basic::SRC_TABLE,
+			'field' => $field,
+			'row'   => $record,
+			'lang'  => $language,
+			'text'  => 'bar'
+		]);
+
+		$backendInstance  = $this->di->get('backendModel');
+		$backendClassName = get_class($backendInstance);
+		$model = \Mocks\Basic::findFirst(1);
+		$manager = \Ovide\Lib\Translate\Adapter\Model\Manager::retrieve(
+				$model,
+				['id'],
+				['backendModel' => $backendClassName]
+		);
+
+		$expected = 'fooo';
+		$manager->set($field, $language, $expected);
+		$this->assertEquals($expected, $manager->get($field, $language));
+
+		$manager->set($field, $language, 'baaar');
+		$this->assertEquals('baaar', $manager->get($field, $language));
+
+		$empty = new \Mocks\Basic();
+		$manager2 = \Ovide\Lib\Translate\Adapter\Model\Manager::retrieve(
+				$empty,
+				['id'],
+				['backendModel' => $backendClassName]
+		);
+		$expected2 = 'bar';
+		$manager2->set($field, $language, $expected2);
+		$this->assertEquals($expected2, $manager2->get($field, $language));
+
+		$I->dropFromDB('translation');
 	}
 
+	/**
+	 * @depends testSet
+	 */
 	public function testPersist()
 	{
+		$I = $this->tester;
 
+		$record   = 1;
+		$field    = 'name';
+		$language = 'en';
+
+		$I->haveInDatabase('basic', ['id' => $record]);
+		$I->haveInDatabase('translation', [
+			'table' => \Mocks\Basic::SRC_TABLE,
+			'field' => $field,
+			'row'   => $record,
+			'lang'  => $language,
+			'text'  => 'bar'
+		]);
+
+		$backendInstance  = $this->di->get('backendModel');
+		$backendClassName = get_class($backendInstance);
+		$model = \Mocks\Basic::findFirst(1);
+		$manager = \Ovide\Lib\Translate\Adapter\Model\Manager::retrieve(
+				$model,
+				['id'],
+				['backendModel' => $backendClassName]
+		);
+
+		$expected = 'fooo';
+		$manager->set($field, $language, $expected);
+		$manager->persist();
+
+		$I->seeInDatabase('translation', [
+			'table' => \Mocks\Basic::SRC_TABLE,
+			'field' => $field,
+			'row'   => $record,
+			'lang'  => $language,
+			'text'  => $expected
+		]);
+
+		$manager->set($field, $language, 'baaar');
+		$manager->set('description', 'es', 'foooo');
+		$manager->set('name', 'es', 'fooobar');
+		$manager->persist(['en' => ['name'], 'es' => ['description']]);
+
+		$I->seeInDatabase('translation', [
+			'table' => \Mocks\Basic::SRC_TABLE,
+			'field' => 'name',
+			'row'   => $record,
+			'lang'  => 'en',
+			'text'  => 'baaar'
+		]);
+
+		$I->dontSeeInDatabase('translation', [
+			'table' => \Mocks\Basic::SRC_TABLE,
+			'field' => 'name',
+			'row'   => $record,
+			'lang'  => 'es',
+			'text'  => 'fooobar'
+		]);
+
+		$I->seeInDatabase('translation', [
+			'table' => \Mocks\Basic::SRC_TABLE,
+			'field' => 'description',
+			'row'   => $record,
+			'lang'  => 'es',
+			'text'  => 'foooo'
+		]);
+
+		$empty = new \Mocks\Basic();
+		$manager2 = \Ovide\Lib\Translate\Adapter\Model\Manager::retrieve(
+				$empty,
+				['id'],
+				['backendModel' => $backendClassName]
+		);
+
+		$manager2->set('name', 'en', 'foofoo');
+		$this->assertFalse($manager2->persist());
+
+		$I->dropFromDB('translation');
 	}
 
 	public function testRemove()
 	{
+		$I = $this->tester;
 
+		$record   = 1;
+		$field    = 'name';
+		$language = 'en';
+
+		$I->haveInDatabase('basic', ['id' => $record]);
+		$I->haveInDatabase('translation', [
+			'table' => \Mocks\Basic::SRC_TABLE,
+			'field' => $field,
+			'row'   => $record,
+			'lang'  => $language,
+			'text'  => 'bar'
+		]);
+
+		$backendInstance  = $this->di->get('backendModel');
+		$backendClassName = get_class($backendInstance);
+		$model = \Mocks\Basic::findFirst(1);
+		$manager = \Ovide\Lib\Translate\Adapter\Model\Manager::retrieve(
+				$model,
+				['id'],
+				['backendModel' => $backendClassName]
+		);
+
+		$manager->remove();
+
+		$I->dontSeeInDatabase('translation', [
+			'table' => \Mocks\Basic::SRC_TABLE,
+			'field' => $field,
+			'row'   => $record,
+			'lang'  => $language,
+			'text'  => 'bar'
+		]);
+
+		$empty = new \Mocks\Basic();
+		$manager2 = \Ovide\Lib\Translate\Adapter\Model\Manager::retrieve(
+				$empty,
+				['id'],
+				['backendModel' => $backendClassName]
+		);
+
+		$this->assertFalse($manager2->remove());
 	}
 }
