b5938f794fe4efe46e66b0ab6ef68633f4668686
Albert Ovide <albert.ovide@igmweb.com>
2014-11-24 11:23:57 +0000

    Add helper 'dropFromDB()'

--- tests/_support/UnitHelper.php
+++ tests/_support/UnitHelper.php
@@ -6,5 +6,34 @@ namespace Codeception\Module;
 
 class UnitHelper extends \Codeception\Module
 {
+    /**
+     * @param string $table
+     * @param array $filter
+     */
+	public function dropFromDB($table, array $filter=null)
+	{
+		$driver = $this->getCon();
 
+		$query = "DELETE FROM `$table`";
+		if ($filter !== null) {
+			$where  = [];
+			foreach ($filter as $key => $value) {
+				$value   = mysql_escape_string($value);
+				$where[] = "`$key` = '$value'";
+			}
+			$query .= 'WHERE '.implode(' AND ', $where);
+		}
+
+		$driver->exec($query);
+	}
+
+	/**
+	 * @return \PDO
+	 */
+	private function getCon()
+	{
+        $config = \Codeception\Configuration::config();
+        $dbConf = $config['modules']['config']['Db'];
+        return \Codeception\Lib\Driver\Db::create($dbConf['dsn'], $dbConf['user'], $dbConf['password'])->getDbh();
+	}
 }
