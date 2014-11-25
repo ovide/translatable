0b4228c5bee80aa8b1943dac0991fa227f2bfb24
Albert Ovide <albert.ovide@igmweb.com>
2014-11-25 15:23:38 +0000

    Add backend mock models

--- tests/functional/BasicTest.php
+++ tests/functional/BasicTest.php
@@ -23,6 +23,7 @@ class BasicTest extends \Codeception\TestCase\Test
 		$this->di = new \Phalcon\DI();
 		$this->di->setShared('modelsManager', \Phalcon\Mvc\Model\Manager::class);
 		$this->di->setShared('modelsMetadata', \Phalcon\Mvc\Model\MetaData\Memory::class);
+		$this->di->setShared('collectionManager', \Phalcon\Mvc\Collection\Manager::class);
 		$this->di->setShared('db', function() {
 			return new \Phalcon\Db\Adapter\Pdo\Mysql([
 				'host'     => 'localhost',
@@ -36,21 +37,32 @@ class BasicTest extends \Codeception\TestCase\Test
 				'host'     => 'localhost',
 				'username' => 'translatable',
 				'password' => 'translatable',
-				'dbname'   => 'translatabl2'
+				'dbname'   => 'translatable2'
 			]);
 		});
+		$this->di->setShared('mongo', function() {
+			$mongo = new \MongoClient('mongodb://127.0.0.1:27017');
+			return $mongo->selectDB("translatable");
+		});
 
-		$mockBackend = $this->getMock(Ovide\Lib\Translate\Adapter\AbstractBackend::class, ['getSource'])
-				->expects($this->any())
-				->method('getSource')
-				->willReturn('translation');
+		$this->di->setShared('translator', function() {
 
-		$this->di->setShared('translator', function() use ($mockBackend) {
-			$backend = get_class($mockBackend);
 			$service = new \Ovide\Lib\Translate\Service();
-			$service->attachAdapter('db', Ovide\Lib\Translate\Model::class, ['backendModel' => $backend]);
-			$service->attachAdapter('db2', Ovide\Lib\Translate\Model::class);
+			$service->attachAdapter('db', Ovide\Lib\Translate\Adapter\Model\Manager::class, [
+				'backendModel' => \Mocks\Translation1::class
+			]);
+			$service->attachAdapter('db', Ovide\Lib\Translate\Adapter\Model\Manager::class, [
+				'backendModel' => \Mocks\Translation2::class
+			]);
+			$service->attachAdapter('mongoTranslator', \Ovide\Lib\Translate\Adapter\Collection\Manager::class);
+			$service->bindModelConnection('mongoTraslator', \Mocks\Table13::class);
 			return $service;
 		});
     }
+
+	public function testSetFirstTranslations()
+	{
+		$t11r1 = new Mocks\Table11();
+		$t11r1->create(['id' => 1, 'name' => 'table11 record1 name']);
+	}
 }
